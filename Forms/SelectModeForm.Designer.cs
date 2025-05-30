56515505e4cece212f7906de127e1690b6fc2d76
yum-anna228 <yumanna2006@gmail.com>
2025-05-30 19:37:05 +0300

    добавление кнопок "выйти из аккаунта" и "посмотреть мою статистику"

--- Forms/SelectModeForm.Designer.cs
+++ Forms/SelectModeForm.Designer.cs
@@ -31,6 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectModeForm));
             btn_2players = new Button();
             btn_3players = new Button();
+            btn_viewStatistics = new Button();
+            btn_Exit = new Button();
             SuspendLayout();
             // 
             // btn_2players
@@ -55,6 +57,28 @@
             btn_3players.TabIndex = 1;
             btn_3players.UseVisualStyleBackColor = false;
             // 
+            // btn_viewStatistics
+            // 
+            btn_viewStatistics.BackColor = Color.Transparent;
+            btn_viewStatistics.FlatAppearance.BorderSize = 0;
+            btn_viewStatistics.FlatStyle = FlatStyle.Flat;
+            btn_viewStatistics.Location = new Point(426, 347);
+            btn_viewStatistics.Name = "btn_viewStatistics";
+            btn_viewStatistics.Size = new Size(343, 47);
+            btn_viewStatistics.TabIndex = 2;
+            btn_viewStatistics.UseVisualStyleBackColor = false;
+            // 
+            // btn_Exit
+            // 
+            btn_Exit.BackColor = Color.Transparent;
+            btn_Exit.FlatAppearance.BorderSize = 0;
+            btn_Exit.FlatStyle = FlatStyle.Flat;
+            btn_Exit.Location = new Point(446, 391);
+            btn_Exit.Name = "btn_Exit";
+            btn_Exit.Size = new Size(342, 47);
+            btn_Exit.TabIndex = 3;
+            btn_Exit.UseVisualStyleBackColor = false;
+            // 
             // SelectModeForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -62,6 +86,8 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
+            Controls.Add(btn_Exit);
+            Controls.Add(btn_viewStatistics);
             Controls.Add(btn_3players);
             Controls.Add(btn_2players);
             DoubleBuffered = true;
@@ -74,5 +100,7 @@
 
         private Button btn_2players;
         private Button btn_3players;
+        private Button btn_viewStatistics;
+        private Button btn_Exit;
     }
 }
